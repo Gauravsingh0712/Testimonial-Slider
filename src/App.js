968ed8aed0f04ee52aeb524c5a9664e0cce08491
Gaurav Singh <sumitgaurav6295@gmail.com>
2024-02-04 22:39:49 +0530

    adjusted height

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const App = () => {
     <div className="flex flex-col wi[100vw] h-[100vh] justify-center items-center bg-gray-200">
       <div className="text-center">
         <h1 className="text-4xl font-bold">Our Testimonial</h1>
-        <div className="bg-violet-400 h-[4px] w-[1/5] mt-1"></div>
+        <div className="bg-violet-400 h-[5px] w-[1/5] mt-1"></div>
         <Testimonial reviews={reviews} />
       </div>
     </div>
